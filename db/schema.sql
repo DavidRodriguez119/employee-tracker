be8047097ea7bfe57269eaaf95ba76f8eff29cd4
DavidRodriguez119 <santiago1.dsrr@gmail.com>
2023-11-17 18:17:54 -0500

    added foreign keys to the tables

--- db/schema.sql
+++ db/schema.sql
@@ -12,11 +12,23 @@ CREATE TABLE departments(
 CREATE TABLE role(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+     REFERENCES departments(id)
+     ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+     REFERENCES role(id)
+     ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+     REFERENCES employee(id)
+     ON DELETE CASCADE
 );
